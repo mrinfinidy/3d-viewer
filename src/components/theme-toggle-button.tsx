68e24d3aaf124a28631e9e17b0c82e0ddaed852c
afkdev8 <mr.infinidy@protonmail.com>
2024-02-26 00:25:27 +0100

    Dark mode and stateful username + pw field

---
+++ src/components/theme-toggle-button.tsx
@@ -0,0 +1,30 @@
+import { motion, AnimatePresence, AnimatePresenceProps } from 'framer-motion';
+import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+const ThemeToggleButton = () => {
+    const { toggleColorMode } = useColorMode();
+
+    return (
+        <AnimatePresence >
+            <motion.div
+                style={{ display: 'inline-block' }}
+                key={useColorModeValue('light', 'dark')}
+                initial={{ y: 20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.15 }}
+            >
+                <IconButton
+                  aria-label="Toggle Theme"
+                  colorScheme="pink"
+                  icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+                  onClick={toggleColorMode as () => void}
+                />
+            </motion.div>
+        </AnimatePresence>
+    );
+};
+
+export default ThemeToggleButton;
+
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,12 +7,25 @@ import {
     FormControl,
     FormLabel,
     Switch,
-    useColorMode,
     useColorModeValue,
 } from '@chakra-ui/react';
+import ThemeToggleButton from '../components/theme-toggle-button';
 
 const Login = () => {
-    const { toggleColorMode } = useColorMode();
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const submitLogin = (username: string, password: string) => {
+        let invalidLoginAlert: string = '';
+        invalidLoginAlert += username === '' ? 'Username is required' : '';
+        invalidLoginAlert += password === '' ? 'Password is required' : '';
+        if (invalidLoginAlert !== '') {
+            alert(invalidLoginAlert);
+        }
+        console.log('username:', username);
+        console.log('password:', password);
+    }
+
     const formBackground = useColorModeValue('gray.100', 'gray.700');
 
     return (
@@ -27,27 +40,29 @@ const Login = () => {
                 <Heading mb={6}>Login</Heading>
                 <Input
                     placeholder="username"
-                    type="text"
                     variant="filled"
                     mb={3}
-                />
+                    onChange={(e) => setUsername(e.target.value)}
+                >
+                </Input>
                 <Input
                     placeholder="********"
                     type="password"
                     variant="filled"
                     mb={6}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
-                <Button colorScheme="pink" mb={6}>
+                <Button 
+                    colorScheme="pink" 
+                    mb={6}
+                    onClick={() => submitLogin(username, password)}
+                >
                     Login
                 </Button>
             </Flex>
-            <Switch
-                id="theme-switch"
-                colorScheme="pink"
-                size="lg"
-                mt={10}
-                onChange={toggleColorMode}
-            />
+            <Flex mt={8}>
+                <ThemeToggleButton />
+            </Flex>
         </Flex>
     );
 };
