00e2ef5d6e951777e2503250daded7309b1197cb
afkdev8 <mr.infinidy@protonmail.com>
2024-02-27 21:56:03 +0100

    Orbit around center of model

--- src/pages/Model.tsx
+++ src/pages/Model.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
 
 interface ModelProps {
     modelPath: string;
@@ -7,7 +8,16 @@ interface ModelProps {
 
 const Model: React.FC<ModelProps> = ({ modelPath }) => {
     const model = useGLTF(modelPath);
-    return <primitive object={model.scene} />;
+
+    const boundingBox = new THREE.Box3().setFromObject(model.scene);
+    // Orbit around the center of the model
+    const boxCenter = new THREE.Vector3();
+    boundingBox.getCenter(boxCenter);
+    model.scene.position.sub(boxCenter);
+
+    return (
+        <primitive object={model.scene} />
+    );
 }
 
 export default Model;
